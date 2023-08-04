8e5ec86f48420dd1deff43d11a15e2cf0e53fa0f
DevShashankSharma <119869630+DevShashankSharma@users.noreply.github.com>
2023-08-04 19:10:28 +0530

    database link change

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const mongoURI = "mongodb+srv://vercel-admin-user:R8FqSPCjPPqTg4Nr@cluster0.a8dkw7k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const mongoURI = "mongodb+srv://vercel-admin-user:R8FqSPCjPPqTg4Nr@cluster0.a8dkw7k.mongodb.net/";
 
 const connectToMongo = async () => {
   await mongoose.connect(mongoURI);
