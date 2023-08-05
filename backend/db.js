4e61bc0154b4b20a214e5bf6226719de657ae222
DevShashankSharma <119869630+DevShashankSharma@users.noreply.github.com>
2023-08-05 07:49:07 +0530

    update

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const mongoURI = "postgresql://admin:6Z9f@2ij2d5k56S@us-east-1.2b68fb6c-9661-42d0-8399-cce21b977b9e.aws.ybdb.io:5433/inotebook?ssl=true&sslmode=verify-full&sslrootcert=root.crt";
+const mongoURI = "postgresql://admin:6Z9f@2ij2d5k56S@us-east-1.2b68fb6c-9661-42d0-8399-cce21b977b9e.aws.ybdb.io:5433/test case?ssl=true&sslmode=verify-full&sslrootcert=root.crt";
 
 const connectToMongo = async () => {
   await mongoose.connect(mongoURI);
