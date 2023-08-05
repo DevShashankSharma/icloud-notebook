694e4d04444b5fc7e77141260efdf07aacca8236
DevShashankSharma <119869630+DevShashankSharma@users.noreply.github.com>
2023-08-05 07:41:58 +0530

    database change

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const mongoURI = "mongodb+srv://vercel-admin-user:R8FqSPCjPPqTg4Nr@cluster0.a8dkw7k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const mongoURI = "postgresql://admin:6Z9f@2ij2d5k56S@us-east-1.2b68fb6c-9661-42d0-8399-cce21b977b9e.aws.ybdb.io:5433/inotebook?ssl=true&sslmode=verify-full&sslrootcert=root.crt";
 
 const connectToMongo = async () => {
   await mongoose.connect(mongoURI);
