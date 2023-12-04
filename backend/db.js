6343807da8254cadf11d4b53d365eaf85269afd1
DevShashankSharma <119869630+DevShashankSharma@users.noreply.github.com>
2023-12-04 22:03:04 +0530

    update

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,19 @@
-const mongoose = require("mongoose");
-const mongoURI = "mongodb+srv://vercel-admin-user:R8FqSPCjPPqTg4Nr@cluster0.a8dkw7k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// const mongoose = require("mongoose");
+// const mongoURI = "mongodb+srv://vercel-admin-user:R8FqSPCjPPqTg4Nr@cluster0.a8dkw7k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-const connectToMongo = async () => {
-  await mongoose.connect(mongoURI);
-  console.log("Connected to Mongo successfully");
-};
+// const connectToMongo = async () => {
+//   await mongoose.connect(mongoURI);
+//   console.log("Connected to Mongo successfully");
+// };
 
-module.exports = connectToMongo;
+// module.exports = connectToMongo;
+
+rules_version = '2';
+
+service cloud.firestore {
+  match /databases/{database}/documents {
+    match /{document=**} {
+      allow read, write: if false;
+    }
+  }
+}
